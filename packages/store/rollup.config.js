aba7b07b2e269d62ae71e75f5d1493b8bdd866e3
Gregor Adams <greg@pixelass.com>
2023-05-03 19:24:14 +0200

    chore: fix build

--- packages/store/rollup.config.js
+++ packages/store/rollup.config.js
@@ -1,23 +1,42 @@
 // Rollup.config.js
-import dts from "rollup-plugin-dts";
 import { swc } from "rollup-plugin-swc3";
-export default {
-	input: "src/index.ts",
-	output: {
-		file: "dist/index.js",
-		format: "es",
+export default [
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.js",
+			format: "es",
+		},
+		plugins: [
+			swc({
+				// All options are optional
+				include: /\.[mc]?[jt]sx?$/, // Default
+				exclude: /node_modules/, // Default
+				tsconfig: "tsconfig.production.json", // Default
+				// tsconfig: false, // You can also prevent `rollup-plugin-swc` from reading tsconfig.json, see below
+				// And add your swc configuration here!
+				// "filename" will be ignored since it is handled by rollup
+				jsc: {},
+			}),
+		],
 	},
-	plugins: [
-		swc({
-			// All options are optional
-			include: /\.[mc]?[jt]sx?$/, // Default
-			exclude: /node_modules/, // Default
-			tsconfig: "tsconfig.production.json", // Default
-			// tsconfig: false, // You can also prevent `rollup-plugin-swc` from reading tsconfig.json, see below
-			// And add your swc configuration here!
-			// "filename" will be ignored since it is handled by rollup
-			jsc: {},
-		}),
-		dts(),
-	],
-};
+	{
+		input: "src/index.ts",
+		output: {
+			file: "dist/index.d.ts",
+			format: "es",
+		},
+		plugins: [
+			swc({
+				// All options are optional
+				include: /\.[mc]?[jt]sx?$/, // Default
+				exclude: /node_modules/, // Default
+				tsconfig: "tsconfig.production.json", // Default
+				// tsconfig: false, // You can also prevent `rollup-plugin-swc` from reading tsconfig.json, see below
+				// And add your swc configuration here!
+				// "filename" will be ignored since it is handled by rollup
+				jsc: {},
+			}),
+		],
+	},
+];
