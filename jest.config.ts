3563727a442d1a87f503c66ac28276ca167bda64
Gregor Adams <greg@pixelass.com>
2023-07-14 23:09:43 +0200

    chore: map paths for jest

--- jest.config.ts
+++ jest.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
 	transformIgnorePatterns: ["node_modules/(?!humanize-string)/"],
 	moduleNameMapper: {
 		"(.+)\\.js": "$1",
+		"^@hyv/utils/(.*)$": "<rootDir>/packages/utils/src/$1",
+		"^@hyv/openai/(.*)$": "<rootDir>/packages/openai/src/$1",
+		"^@hyv/store/(.*)$": "<rootDir>/packages/store/src/$1",
+		"^@hyv/stable-diffusion/(.*)$": "<rootDir>/packages/stable-diffusion/src/$1",
+		"^@hyv/core/(.*)$": "<rootDir>/packages/core/src/$1",
 	},
 	extensionsToTreatAsEsm: [".ts"],
 };
