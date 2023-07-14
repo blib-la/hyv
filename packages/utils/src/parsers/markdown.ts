8d445c40e7b2c8553f96ca140caba465553d49b0
Gregor Adams <greg@pixelass.com>
2023-07-14 22:45:01 +0200

    fix(utils): handles non headline content

--- packages/utils/src/parsers/markdown.ts
+++ packages/utils/src/parsers/markdown.ts
@@ -5,12 +5,15 @@ import { splitBy } from "../string.js";
 export function parseMarkdown<T>(markdown: string, pattern = /^#+ /m) {
 	return splitBy(markdown.trim(), pattern).reduce((previousValue, currentValue) => {
 		const [key, ...lines] = currentValue.split("\n");
-		return {
-			...previousValue,
-			[camelcase(key)]: lines
-				.map(line => line.trim())
-				.filter(Boolean)
-				.join("\n"),
-		};
+
+		return lines.length
+			? {
+					...previousValue,
+					[camelcase(key)]: lines
+						.map(line => line.trim())
+						.filter(Boolean)
+						.join("\n"),
+			  }
+			: previousValue;
 	}, {}) as T;
 }
